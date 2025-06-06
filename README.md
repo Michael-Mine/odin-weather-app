50aeed744dc89b3f965abc2ba0f300af7c5a7704
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2025-06-06 19:17:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mrmine-spa
+
+My personal portfolio website using HTML, CSS and JavaScipt as a Single Page Application.
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+import "./form.css";
+import { usePageScroller } from "@furman1331/page-scroller";
+
+usePageScroller().initPageScroller("#main");
