b698aef04a167ff3ad55150579f2275b7843aa0c
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2025-06-07 12:33:54 +0100

    Add async function to get weather json data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import "./styles.css";
 
+async function getWeatherFullData(location) {
+  const url =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+    location +
+    "?key=KGNPQ92K2A2D2GK96CV98SB3F";
+  try {
+    const response = await fetch(url, { mode: "cors" });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(weatherData);
+    return json;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+let fullWeatherData = getWeatherFullData("london");
+
+function processWeatherData() {
+  let appWeatherData = {
+    location: fullWeatherData.resolvedAddress,
+    description: fullWeatherData.description,
+  };
+  console.log(appWeatherData);
+}
+
+processWeatherData();
